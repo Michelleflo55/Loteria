6f556d29716bde0459b7761cbcf2bfd10e8070e2
Michelle Flores <lmfg2456@gmail.com>
2022-03-08 14:23:29 -0800

    added pictures

--- script.js
+++ script.js
@@ -11,21 +11,21 @@
 //how to randomize js array
 ///////////SETTING UP PICTURE RANDOMIZATION
 let picArr = [
-        { originalPosition: 0, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.eFOyDJbGyhvER8hLwcJAVAHaDt%26pid%3DApi&f=1' },
-        { originalPosition: 1, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 2, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 3, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 4, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 5, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 6, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 7, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 8, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 9, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 10, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 11, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 12, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 13, clicked: false, img: 'www.exampleimageurl.com' },
-        { originalPosition: 14, clicked: false, img: 'www.exampleimageurl.com' }
+        { originalPosition: 0, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.s-zGgVVdiWlnr6VGqZcNJQHaLG%26pid%3DApi&f=1' },
+        { originalPosition: 1, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.kWGzGhAXn-FaVOXkeN2WoAHaLG%26pid%3DApi&f=1' },
+        { originalPosition: 2, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Wy_iDckBn3gieltZzLEw-gHaLG%26pid%3DApi&f=1' },
+        { originalPosition: 3, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.explicit.bing.net%2Fth%3Fid%3DOIP.-MH4PvpGF1Mxqwygckf0nQHaKX%26pid%3DApi&f=1' },
+        { originalPosition: 4, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-6WOIGE-7UxCjL22r-ASEwHaJ4%26pid%3DApi&f=1' },
+        { originalPosition: 5, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.explicit.bing.net%2Fth%3Fid%3DOIP.sPo18Z4L-Vpp1TeEUJ2nJQHaLG%26pid%3DApi&f=1' },
+        { originalPosition: 6, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Kj7TeS1OPJk5pFR0wRHNRgHaK0%26pid%3DApi&f=1' },
+        { originalPosition: 7, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.wfYl6UQQmka_GmMzvS-wtwHaK1%26pid%3DApi&f=1' },
+        { originalPosition: 8, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.cl722HZ2q1b5ABvJKnkhPgHaLG%26pid%3DApi&f=1' },
+        { originalPosition: 9, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.ZDpGgHgg8dp5nxlFSunSEAHaLG%26pid%3DApi&f=1' },
+        { originalPosition: 10, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.53IL0FO-H7bcIcH906cjowHaLH%26pid%3DApi&f=1' },
+        { originalPosition: 11, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.m5GoqhCgYT_-_dNuGV8WWAHaLp%26pid%3DApi&f=1' },
+        { originalPosition: 12, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.G4Y1nQnKGE70LCL1CrfW1gHaJQ%26pid%3DApi&f=1' },
+        { originalPosition: 13, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Hr0oAeCOudeVJ-Pc7xQAHwHaLj%26pid%3DApi&f=1' },
+        { originalPosition: 14, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qrxl5YVebK0U0k3JA4TVnQHaLG%26pid%3DApi&f=1' }
       ]
 // I found this Fisher-Yates solution here: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
       let shuffle = (array) => {
@@ -44,3 +44,4 @@ let picArr = [
                         // Used like so
                 shuffle(picArr);
                 console.log(picArr);
+               
