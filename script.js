13d5911fe3c3adac510de6016c461e4ae7ec8182
Michelle Flores <lmfg2456@gmail.com>
2022-03-08 07:37:57 -0800

    Loteria

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
  //redirecting to second html page   
- document.querySelector('#redirect')
+ document.querySelector('button')
  .addEventListener('click', () => {
      window.location.href = 'file:///Users/bubbies/ga_seir/projects/Loteria/play.html';
  });
-
-        console.log('hello')
