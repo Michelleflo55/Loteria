9c5356059b34bea3bcd8804165d3da9078479f41
Michelle Flores <lmfg2456@gmail.com>
2022-03-09 08:44:42 -0800

    new drawing button

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
  //redirecting to second html page   
- document.querySelector('button')
- .addEventListener('click', () => {
-     window.location.href = 'file:///Users/bubbies/ga_seir/projects/Loteria/play.html';
- });
-//example
-// let exArr = [
-// {original position (0 change for each of the 14 )clicked:false, img:'www.exampleimgurl.com'}//paste 15 times add commas in between them all
-//  ]
-// console.log the array
-//how to randomize js array
-///////////SETTING UP PICTURE RANDOMIZATION
+        document.querySelector('button')
+                addEventListener('click', () => {
+                window.location.href = 'file:///Users/bubbies/ga_seir/projects/Loteria/play.html';
+                });
+
+////SETTING UP PICTURE RANDOMIZATION
 let picArr = [
         { originalPosition: 0, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.s-zGgVVdiWlnr6VGqZcNJQHaLG%26pid%3DApi&f=1' },
         { originalPosition: 1, clicked: false, img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.kWGzGhAXn-FaVOXkeN2WoAHaLG%26pid%3DApi&f=1' },
@@ -44,4 +39,11 @@ let picArr = [
                         // Used like so
                 shuffle(picArr);
                 console.log(picArr);
-               
+
+/////// Adding button to shuffle cards
+        document.querySelector('button')
+                addEventListener('click', () => {
+                       
+                });
+
+                console.log('card is drawing')
